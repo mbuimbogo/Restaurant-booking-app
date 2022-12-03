b6cc09385017d92f4c2d24560816d4e1b7243822
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-12-03 15:50:46 +0300

    Seed data to db and run migrations

--- db/migrate/20221203121609_create_bookings.rb
+++ db/migrate/20221203121609_create_bookings.rb
@@ -2,6 +2,8 @@ class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.string :condition
+      t.belongs_to :user, null:false, foreign_key:true
+      t.belongs_to :restaurant, null:false, foreign_key:true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding data'
+
+
+user = User.create(name: "Sophia Nganga", username: "Kabda")
+first = User.create(name: "Verah Njenga", username: "Vee")
+second = User.create(name: "Nana Owiti", username: " Vin")
+
+#restaurants seed
+balk = Restaurant.create(name: "Dhaba Resto", city: "Nairobi", state: "Kenya")
+cruise = Restaurant.create(name: "Kona baridi", city: "Kiserian", state: "Kenya")
+
+Booking.create("condition": "booked", "user_id":user.id, "restaurant_id":balk.id)
+Booking.create("condition": "not_booked", "user_id":user.id, "restaurant_id":balk.id)
+Booking.create("condition": "not_booked", "user_id":first.id, "restaurant_id":cruise.id)
+Booking.create("condition": "not_booked", "user_id":second.id, "restaurant_id":balk.id)
+
+puts 'Done!'
